681c10850e236f5cb6165c9674567c2bf4e36f8c
yeasin-hossaom <yeasinhossain475@gmail.com>
2021-02-13 12:17:39 +0600

    solved

--- main.js
+++ main.js
@@ -49,11 +49,11 @@ const fetchIssues = () => {
 
 	for (var i = 0; i < issues.length; i++) {
 		const { id, description, severity, assignedTo, status } = issues[i];
-
+		const p = `<del>${description} </del>`;
 		issuesList.innerHTML += `<div class="well">
                               <h6>Issue ID: ${id} </h6>
                               <p><span class="label label-info"> ${status} </span></p>
-                              <h3> ${description} </h3>
+                              <h3> ${status == 'Closed' ? p : description} </h3>
                               <p><span class="glyphicon glyphicon-time"></span> ${severity}</p>
                               <p><span class="glyphicon glyphicon-user"></span> ${assignedTo}</p>
                               <a href="#" onclick="setStatusClosed(${id})" class="btn btn-warning">Close</a>
